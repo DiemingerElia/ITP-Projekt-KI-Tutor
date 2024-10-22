98af6c0be66361a55aa53b03da698d1422d6a0ba
Tobias Riener <tobias.riener@htlwy.at>
2024-10-22 17:31:31 +0200

    added function to display only 1 main Element

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-//Navbar buttons
+//Navbar
+const navHomeButton = document.querySelector("main#home");
+
+document.body.onload = function(){
+    let mainElements = document.querySelectorAll("main");
+    mainElements.forEach((HTMLElement) => {
+        console.log(HTMLElement);
+        HTMLElement.style.display = "none";
+    });
+    document.querySelector("main#home").style.display = "block";
+}
