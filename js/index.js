ba3ba1b09276bd8ed88d18e30c50151d6dcd7487
Tobias Riener <tobias.riener@htlwy.at>
2024-11-12 16:06:08 +0100

    added themeswitcher and started localStorage

--- js/index.js
+++ js/index.js
@@ -3,9 +3,7 @@ const navHomeButton = document.querySelector("#navHome");
 const navAboutButton = document.querySelector("#navAbout");
 const navContactButton = document.querySelector("#navContact");
 const navTutorButton = document.querySelector("#navTutor");
-const tutorTypes = document.getElementsByName('option');
-
-const tutorChats = {};
+const themeSwitcher = document.querySelector('.themeSwitcher input');
 
 const navButtons = [navHomeButton, navAboutButton, navContactButton, navTutorButton];
 let currentPage = "home";
@@ -23,6 +21,16 @@ navButtons.forEach((element) => {
     });
 });
 
+themeSwitcher.addEventListener('click', function (event) {
+    if(themeSwitcher.checked){
+        document.body.classList.remove('lightheme');
+        document.body.classList.add('darktheme');
+    }
+    else{
+        document.body.classList.remove('darktheme');
+        document.body.classList.add('lightheme');
+    }
+});
 
 document.body.onload = function(){
     let mainElements = document.querySelectorAll("main");
@@ -32,14 +40,49 @@ document.body.onload = function(){
     document.querySelector("main#home").style.display = "block";
 }
 
+//  Tutor Specific Functions
+const tutorLabels = document.getElementsByName('ki-tutor');
+let tutorTypes = [];
+for(let i = 0; i < tutorLabels.length; i++){
+    tutorTypes.push(tutorLabels[i].innerHTML);
+}
+
+const tutorChoice = document.getElementsByName('option');
+const tutorChats = {};
+
 function LoadChats(){
-    for(let i = 0; i < tutorTypes.clientHeight; i++){
+    for(let i = 0; i < tutorTypes.length; i++){
         tutorChats[tutorTypes[i]] = localStorage.getItem(tutorTypes[i]);
+        if(tutorChats[tutorTypes[i]] === undefined){
+            tutorChats[tutorTypes[i]] = { 'tutor': tutorTypes[i], };
+        }
     }
 }
 
 function SaveChats(){
-    for(let i = 0; i < tutorChats.length; i++){
-        localStorage.setItem(tutorChats[i].key, tutorChats[i].value);
+    for(let i = 0; i < Object.keys(tutorChats).length; i++){
+        let currentkey = Object.keys(tutorChats)[i];
+        localStorage.setItem(currentkey, JSON.stringify(tutorChats[currentkey]));
     }
+}
+
+function ShowChatForTutor(tutor){
+    const chatElement = document.querySelector('chatElement');
+    tutorKeys = Object.keys(tutorChats[tutor]);
+    for(let i = 0; i < tutorKeys.length; i++){
+        let paragraph = document.createElement("p");
+        paragraph.innerHTML = tutorChats[tutor][tutorKeys[i]];
+
+        if(tutorKeys[i].includes("KI")){
+            paragraph.className = "ki-message";
+        }
+        else{
+            paragraph.className = "user-message";
+        }
+        chatElement.appendChild(paragraph);
+    }
+}
+
+function SendMessage(tutor, message){
+    tutorChats[tutor]["KI-" ]
 }
