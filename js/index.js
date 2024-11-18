37be2200958880406cb609d804f2152777f29d40
Tobias Riener <tobias.riener@htlwy.at>
2024-11-18 15:06:57 +0100

    updated page switching for nav and footer

--- js/index.js
+++ js/index.js
@@ -3,10 +3,15 @@ const navHomeButton = document.querySelector("#navHome");
 const navAboutButton = document.querySelector("#navAbout");
 const navContactButton = document.querySelector("#navContact");
 const navTutorButton = document.querySelector("#navTutor");
+const burgerMenu = document.querySelector('#burgermenu');
+
+const footerTermsOfUse = document.querySelector('#footerTermsOfUse');
+const footerLegal = document.querySelector('#footerLegal');
 
 const themeSwitcher = document.querySelector('.themeSwitcher input');
 
 const navButtons = [navHomeButton, navAboutButton, navContactButton, navTutorButton];
+const footerLinks = [footerTermsOfUse, footerLegal];
 let currentPage = "home";
 
 navButtons.forEach((element) => {
@@ -20,8 +25,36 @@ navButtons.forEach((element) => {
     
         currentPage = newPage;
         document.title = document.querySelector('#nav' + newPage.charAt(0).toUpperCase() + newPage.slice(1)).innerHTML.split('</i>').pop().trim();
+
+        if(window.innerWidth <= 768){
+            document.querySelector('#header > nav').style.display = "none";
+        }
     });
 });
+
+footerLinks.forEach((element) => {
+    element.addEventListener("click", (event) => {
+        let newPage = event.target.id.toString().toLocaleLowerCase().replace("footer", "");
+
+        if(currentPage == newPage) return;
+
+        document.querySelector("#" + newPage).style.display = "block";
+        document.querySelector("#" + currentPage).style.display = "none";
+    
+        currentPage = newPage;
+        document.title = document.querySelector('#footer' + newPage.charAt(0).toUpperCase() + newPage.slice(1)).innerHTML.split('</i>').pop().trim();
+    });
+});
+
+burgerMenu.addEventListener('click', function(event){
+    event.preventDefault();
+    if(document.querySelector('#header > nav').style.display == "block"){
+        document.querySelector('#header > nav').style.display = "none";
+    }
+    else{
+        document.querySelector('#header > nav').style.display = "block"
+    }
+})
 
 themeSwitcher.addEventListener('change', function (event) {
     if(themeSwitcher.checked){
@@ -53,3 +86,16 @@ document.body.onload = function(){
     }
     APILoad();
 }
+
+window.onresize = function(){
+    if(window.innerWidth >= 768) {
+        document.querySelector('#header > nav').style.display = "";
+    }
+};
+
+document.querySelector('#tinputarea').addEventListener('keypress', function(event){
+    if(event.key === "Enter"){
+        event.preventDefault();
+        document.querySelector('#tinputsend').click();
+    }
+});
